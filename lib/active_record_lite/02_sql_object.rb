eb423167d735a68796787ac6330926e45baa3bbb
Joshua Meisel <joshua.a.meisel@gmail.com>
2014-05-23 15:37:29 -0400

    Implement find, all, and insert (the latter w/o updating id)

--- lib/active_record_lite/02_sql_object.rb
+++ lib/active_record_lite/02_sql_object.rb
@@ -5,7 +5,6 @@ require 'active_support/inflector'
 class MassObject
   def self.parse_all(results)
     results.map do |attributes|
-      puts self.class
       self.new(attributes)
     end
   end
@@ -21,29 +20,38 @@ class SQLObject < MassObject
   end
   
   def self.columns
-    DBConnection.execute2("SELECT * FROM " + self.table_name)
+    return @columns if @columns
+    
+    @columns = DBConnection.execute2("SELECT * FROM " + self.table_name)
       .first
       .map(&:to_sym)
-  end
-  
-  if self.superclass == SQLObject
-    self.columns.each do |col|
+    @columns.each do |col|
       define_method(col) do
         attributes[col]
       end
-    
+
       define_method(col.to_s + "=") do |val|
         attributes[col] = val
       end
     end
+    
+    @columns
   end
   
   def self.all
-    # ...
+    @all if @all
+    @all = self.parse_all(
+    DBConnection.execute("SELECT * FROM " + self.table_name)
+    )
   end
 
   def self.find(id)
-    # ...
+    self.parse_all(
+      DBConnection.execute(
+        "SELECT * FROM " + self.table_name + 
+        " WHERE id = " + id.to_s
+      )
+    ).first
   end
 
   def attributes
@@ -51,15 +59,35 @@ class SQLObject < MassObject
   end
 
   def insert
-    # ...
+    column_string = "(id"
+    value_string = "("
+    self.class.columns.each do |col|
+      next if col == :id
+      column_string.concat("#{col.to_s},")
+      value = attributes[col]
+      if value.is_a?(String)
+        value_str = "'#{value}'"
+      else
+        value_str = value.to_s
+      end
+      
+      value_string.concat("#{value_str},")
+    end
+    
+    [column_string, value_string].each {|str| str.chop!.concat(")")}
+    
+    DBConnection.execute(
+      "INSERT INTO #{self.class.table_name} #{column_string} " +
+      "VALUES #{value_string}"
+    )
   end
 
-  def initialize(params = {})
+  def initialize(params = {})    
     params.each do |attr_name, val|
-      unless self.class.columns.includes?(attr_name.to_sym)
+      unless self.class.columns.include?(attr_name.to_sym)
         raise "unknown attribute '#{attr_name}'" 
       end
-      @attributes[attr_name.to_sym = val]
+      attributes[attr_name.to_sym] = val
     end
   end
 
@@ -72,6 +100,8 @@ class SQLObject < MassObject
   end
 
   def attribute_values
-    # ...
+    [].tap do |values|
+      self.class.columns.each {|col| values << attributes[col]}
+    end
   end
 end
