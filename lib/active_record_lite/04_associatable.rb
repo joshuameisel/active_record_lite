89e6d591bbb84e5279b8f765c7ae011ce8ec9909
Joshua Meisel <joshua.a.meisel@gmail.com>
2014-05-23 21:16:39 -0400

    Begin to implement associatable

--- lib/active_record_lite/04_associatable.rb
+++ lib/active_record_lite/04_associatable.rb
@@ -10,30 +10,52 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize 
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    options.each do |option, value|
+      instance_variable_set("@" + option.to_s, value)
+    end
+    
+    @class_name  ||= name.to_s.camelcase
+    @primary_key ||= :id
+    @foreign_key ||= "#{name.to_s}_id".to_sym
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    options.each do |option, value|
+      instance_variable_set("@" + option.to_s, value)
+    end
+    
+    @class_name  ||= name.to_s.singularize.camelcase
+    @primary_key ||= :id
+    @foreign_key ||= "#{self_class_name.downcase}_id".to_sym
   end
 end
 
 module Associatable
   # Phase IVb
   def belongs_to(name, options = {})
-    # ...
+    instance_variable.set("@" + name.to_s, BelongsToOptions.new(name, options))
+    has_many_options = instance_variable.get("@" + name.to_s)
+    define_method(name) do
+      DBConnection.execute([<<-SQL, 
+      SELECT 
+        * 
+      FROM 
+        ?
+      WHERE
+        ?=?
+      SQL
   end
 
   def has_many(name, options = {})
@@ -46,5 +68,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  include Associatable
 end
