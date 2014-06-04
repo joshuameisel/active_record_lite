14c07fd16c8713a7fcd01e7e345defdc7504b040
App Academy Student <student@appacademy.io>
2014-06-04 15:37:04 -0400

    Implement Phase V (has_one_through)

--- lib/active_record_lite/05_associatable2.rb
+++ lib/active_record_lite/05_associatable2.rb
@@ -5,9 +5,24 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    
     define_method(name) do
-      through_options = self.class
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      sql =
+        "SELECT
+          #{source_options.table_name}.*
+        FROM
+          #{through_options.table_name}
+        JOIN
+          #{source_options.table_name} ON
+            #{through_options.table_name}.#{source_options.foreign_key} =
+              #{source_options.table_name}.#{source_options.primary_key}
+        WHERE
+          #{through_options.table_name}.#{through_options.primary_key} = ?"
+
+      attrs = DBConnection.execute(sql, self.send(through_options.foreign_key)).first
+      source_options.model_class.new(attrs)
     end
   end
 end
